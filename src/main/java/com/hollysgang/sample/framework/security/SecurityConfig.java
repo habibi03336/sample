f5867301135c1960bc002b2c984fcf5051bda295
하지훈 <jnh03336@gmail.com>
2025-01-10 11:13:14 +0900

    feat: 커스텀 security filter chain 등록

---
+++ src/main/java/com/hollysgang/sample/framework/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.hollysgang.sample.framework.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+        http
+            .authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+            )
+        ;
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
