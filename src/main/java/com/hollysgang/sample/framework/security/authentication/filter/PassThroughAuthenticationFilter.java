6851355844406990bdc6b4a743cf4bc28488e66a
하지훈 <jnh03336@gmail.com>
2025-01-10 15:22:20 +0900

    feat: 무지성 인증해주는 인증 필터

---
+++ src/main/java/com/hollysgang/sample/framework/security/authentication/filter/PassThroughAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.hollysgang.sample.framework.security.authentication.filter;
+
+import com.hollysgang.sample.framework.security.authentication.token.pre.NullToken;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+public class PassThroughAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public PassThroughAuthenticationFilter(AntPathRequestMatcher filterTargetUrl,
+                                           AuthenticationManager authenticationManager) {
+        super(filterTargetUrl, authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return this.getAuthenticationManager().authenticate(new NullToken());
+    }
+}
