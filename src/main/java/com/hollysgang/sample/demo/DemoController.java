4c3c483537bae27e0475775ad9fc947a928dbced
하지훈 <jnh03336@gmail.com>
2025-01-10 11:17:55 +0900

    feat: demo api to access

---
+++ src/main/java/com/hollysgang/sample/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.hollysgang.sample.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Home Sweet Home";
+    }
+
+}
