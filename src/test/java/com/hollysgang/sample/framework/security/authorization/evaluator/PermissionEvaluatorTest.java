7f56b7b69c44e768e748057fae15e081ec0ca077
하지훈 <jnh03336@gmail.com>
2025-01-12 15:55:13 +0900

    test: deny 케이스 추가

--- src/test/java/com/hollysgang/sample/framework/security/authorization/evaluator/PermissionEvaluatorTest.java
+++ src/test/java/com/hollysgang/sample/framework/security/authorization/evaluator/PermissionEvaluatorTest.java
@@ -1,5 +1,6 @@
 package com.hollysgang.sample.framework.security.authorization.evaluator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
@@ -11,16 +12,17 @@ import java.util.*;
 class PermissionEvaluatorTest {
 
     @Test
-    public void testValidAccess(){
+    @DisplayName("ACCESS 케이스")
+    public void testAccess(){
         // given
         APIPermissionLoader apiPermissionLoader = () -> {
-                APIPermissionInfo apiPermissionInfo = new APIPermissionInfo("/api/case", HttpMethod.GET);
+                APIPermissionInfo apiPermissionInfo = new APIPermissionInfo("/api/local-case", HttpMethod.GET);
                 apiPermissionInfo.addAllowedRoles("ROLE_LOCAL_INVESTIGATOR");
                 return List.of(apiPermissionInfo);
         };
         PermissionEvaluator permissionEvaluator = new PermissionEvaluator(apiPermissionLoader);
         MockHttpServletRequest request = new MockHttpServletRequest();
-        request.setRequestURI("/api/case");
+        request.setRequestURI("/api/local-case");
         request.setMethod(HttpMethod.GET.name());
 
         // execute
@@ -28,6 +30,27 @@ class PermissionEvaluatorTest {
 
         // expect
         assertThat(pr).isEqualTo(PermissionResult.ACCESS);
+    }
+
+    @Test
+    @DisplayName("DENY 케이스")
+    public void testDenied(){
+        // given
+        APIPermissionLoader apiPermissionLoader = () -> {
+            APIPermissionInfo apiPermissionInfo = new APIPermissionInfo("/api/nation-case", HttpMethod.GET);
+            apiPermissionInfo.addAllowedRoles("ROLE_HEADQUARTER_INVESTIGATOR");
+            return List.of(apiPermissionInfo);
+        };
+        PermissionEvaluator permissionEvaluator = new PermissionEvaluator(apiPermissionLoader);
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setRequestURI("/api/nation-case");
+        request.setMethod(HttpMethod.GET.name());
+
+        // execute
+        PermissionResult pr = permissionEvaluator.evaluate(request, "ROLE_LOCAL_INVESTIGATOR");
+
+        // expect
+        assertThat(pr).isEqualTo(PermissionResult.DENIED);
     }
 
 
