aabb53560126a7bc58defc9f8a8dce97b5d9ce70
하지훈 <jnh03336@gmail.com>
2025-01-14 23:12:45 +0900

    feat: core의 APIPermissionRepository 인터페이스 구현

---
+++ src/main/java/com/hollysgang/sample/framework/module/api/APIPermissionRepositoryImpl.java
@@ -0,0 +1,72 @@
+package com.hollysgang.sample.framework.module.api;
+
+import com.hollysgang.sample.demo.entity.Menu;
+import com.hollysgang.sample.framework.core.manage.api.APIPermission;
+import com.hollysgang.sample.framework.core.manage.api.APIPermissionRepository;
+import com.hollysgang.sample.framework.module.api.entity.API;
+import com.hollysgang.sample.framework.module.api.entity.MenuAPI;
+import com.hollysgang.sample.framework.module.api.repository.MenuAPIRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class APIPermissionRepositoryImpl implements APIPermissionRepository {
+
+    private final MenuAPIRepository menuAPIRepository;
+
+    public APIPermissionRepositoryImpl(MenuAPIRepository menuAPIRepository) {
+        this.menuAPIRepository = menuAPIRepository;
+    }
+
+    @Override
+    public List<APIPermission> findAllByApikey(String apikey) {
+        return menuAPIRepository.findAllByApiId(Long.parseLong(apikey)).stream()
+                .map(this::mapToApiPermission)
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    public List<APIPermission> findAllByAuthorizedEntity(String authorizedEntity) {
+        return menuAPIRepository.findAllByMenuId(Long.parseLong(authorizedEntity)).stream()
+                .map(this::mapToApiPermission)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<APIPermission> findAll() {
+        return menuAPIRepository.findAll().stream().map(this::mapToApiPermission).collect(Collectors.toList());
+    }
+
+    @Override
+    public APIPermission findByAuthorizedEntityAndApikey(String authorizedEntity, String apiKey) {
+        Optional<MenuAPI> menuApi = menuAPIRepository.findByMenuIdAndApiId(Long.parseLong(authorizedEntity),
+                Long.parseLong(apiKey));
+        if(menuApi.isEmpty()) return null;
+        return mapToApiPermission(menuApi.get());
+    }
+
+    @Override
+    public void addPermission(String authorizedEntity, String apiKey) {
+        Menu menu = Menu.builder().id(Long.parseLong(authorizedEntity)).build();
+        API api = API.builder().id(Long.parseLong(apiKey)).build();
+        MenuAPI menuApi = MenuAPI.builder().menu(menu).api(api).build();
+        menuAPIRepository.save(menuApi);
+    }
+
+    @Override
+    public void deletePermission(String authorizedEntity, String apiKey) {
+        Optional<MenuAPI> menuApi = menuAPIRepository.findByMenuIdAndApiId(Long.parseLong(authorizedEntity),
+                Long.parseLong(apiKey));
+        if(menuApi.isEmpty()) return;
+        menuAPIRepository.delete(menuApi.get());
+    }
+
+    private APIPermission mapToApiPermission(MenuAPI menuAPI){
+        return APIPermission.builder()
+                .apiKey(menuAPI.getApi().getId().toString())
+                .authorizedEntity(menuAPI.getMenu().getId().toString())
+                .build();
+    }
+}
---
+++ src/main/java/com/hollysgang/sample/framework/module/api/repository/MenuAPIRepository.java
@@ -0,0 +1,25 @@
+package com.hollysgang.sample.framework.module.api.repository;
+
+import com.hollysgang.sample.framework.module.api.entity.MenuAPI;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface MenuAPIRepository extends JpaRepository<MenuAPI, Long> {
+
+    @Query("SELECT m FROM MenuAPI m WHERE m.menu.id = :menuId AND m.api.id = :apiId")
+    Optional<MenuAPI> findByMenuIdAndApiId(@Param("menuId") Long menuId, @Param("apiId") Long apiId);
+
+    @Query("SELECT m FROM MenuAPI m WHERE m.menu.id = :menuId")
+    List<MenuAPI> findAllByMenuId(@Param("menuId") Long menuId);
+
+    @Query("SELECT m FROM MenuAPI m WHERE m.api.id = :apiId")
+    List<MenuAPI> findAllByApiId(@Param("apiId") Long apiId);
+
+}
